0e59164c1954613a683af32914994b4c4e33d604
Mikhail Lesin <mlesin@mrpost.ru>
2020-07-31 12:22:15 +0300

    chore: Get rid of cli pkgs in favor of using npx

--- package-scripts.js
+++ package-scripts.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 module.exports = {
   scripts: {
-    "build:readme": "pkg-to-readme --template ./readmeTemplate.ejs --force && documentation readme src/** --markdown-toc=false --section API && doctoc README.md",
+    "build:readme": "npx pkg-to-readme --template ./readmeTemplate.ejs --force && npx documentation readme src/** --markdown-toc=false --section API && npx doctoc README.md",
     "build:src:bundle": "rollup -c ../../rollup.config.js",
     "build:src:clean": "rm -rfv dist compat",
     "build:src": "nps 'build:src:clean' 'build:src:bundle'",
